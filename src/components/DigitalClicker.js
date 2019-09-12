a1bb40241a00e7b6c3cb0566d59f6234ccc1549a
Jason Leach <jlspursfan@gmail.com>
2019-09-12 14:22:59 -0700

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,24 @@
 // Code DigitalClicker Component Here
+import React from 'react'
+
+export default class DigitalClicker extends React.Component {
+  constructor() {
+    super();
+  this.state = {
+    timesClicked: 0
+  }
+}
+
+  handleClick() {
+    let newVal = this.state.timesClicked += 1
+    this.setState({
+      timesClicked: newVal
+    })
+  }
+
+  render() {
+    return(
+     <button onClick={this.handleClick.bind(this)}>{this.state.timesClicked}</button>
+    )
+  }
+}
